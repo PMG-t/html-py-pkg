df429c841bc5405c2cd87a3e0bcec57b5e066d0f
PimpMyGit <tommasoredaelli276@gmail.com>
2025-02-27 18:19:08 +0100

    template

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+VERSION = '1.0.0'
+PACKAGE_NAME = "html-py-pkg"
+AUTHOR = "Tommaso Redaelli"
+EMAIL = "tommasoredaelli276@gmail.com"
+GITHUB = f"https://github.com/PMG-t/html-py-pkg.git"
+DESCRIPTION = "Query a db in a chatbot like way"
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    license='MIT',
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    url=GITHUB,
+    packages=setuptools.find_packages("src"),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+    },
+    install_requires=[
+        
+    ]
+)
---
+++ src/html_py_pkg/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+template_folder = os.path.join(os.path.dirname(__file__), 'templates')
